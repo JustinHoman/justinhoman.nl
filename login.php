6c55e99e6a39c1326b97f691268264cf54e7977e
Justin Homan <justinhoman@hotmail.com>
2016-12-05 09:32:55 +0100

    Started with the login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Justin Homan</title>
+  </head>
+  <body>
+    <header>
+      <div class="left">
+        <h1 class="header">www.justinhoman.nl</h1><b>/login</b>
+      </div>
+      <div class="right">
+        <nav>
+          <ul class="nav">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="brandweer.html">Brandweer</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a class="active" href="#">Login</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  </body>
+</html>
